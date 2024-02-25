5dd546ad1f9a03593718fbb83ccf31ecbce2f7d6
Samanta DA SILVA <developpement.sam@gmail.com>
2024-02-25 14:30:39 +0100

    Last updates test and Dashboard methods with counter

--- Billed-app-FR-Front-main/src/containers/Dashboard.js
+++ Billed-app-FR-Front-main/src/containers/Dashboard.js
@@ -95,17 +95,16 @@ export default class {
   };
 
   handleEditTicket(e, bill, bills) {
-    if (this.id === undefined || this.id !== bill.id) {
-      this.id = bill.id;
-      this.isBillDisplayed = true;
-    }
-    if (this.isBillDisplayed) {
+    if (this.counter === undefined || this.id !== bill.id) this.counter = 0;
+    if (this.id === undefined || this.id !== bill.id) this.id = bill.id;
+    if (this.counter % 2 === 0) {
       bills.forEach((b) => {
         $(`#open-bill${b.id}`).css({ background: "#0D5AE5" });
       });
       $(`#open-bill${bill.id}`).css({ background: "#2A2B35" });
       $(".dashboard-right-container div").html(DashboardFormUI(bill));
       $(".vertical-navbar").css({ height: "150vh" });
+      this.counter++;
     } else {
       $(`#open-bill${bill.id}`).css({ background: "#0D5AE5" });
 
@@ -113,8 +112,8 @@ export default class {
         <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
       `);
       $(".vertical-navbar").css({ height: "120vh" });
+      this.counter++;
     }
-    this.isBillDisplayed = !this.isBillDisplayed;
     $("#icon-eye-d").click(this.handleClickIconEye);
     $("#btn-accept-bill").click((e) => this.handleAcceptSubmit(e, bill));
     $("#btn-refuse-bill").click((e) => this.handleRefuseSubmit(e, bill));
@@ -156,6 +155,7 @@ export default class {
     }
 
     bills.forEach((bill) => {
+      $(`#open-bill${bill.id}`).off("click");
       $(`#open-bill${bill.id}`).click((e) =>
         this.handleEditTicket(e, bill, bills)
       );
